39c604a39efd74b7ec41f7820695159e595635ba
bigjou <stas.mishchenko@gmail.com>
2017-04-06 02:30:07 +0200

    First OOP (class Point.cs + list)

---
+++ Snake OOP/Snake OOP/Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_OOP
+{
+    class Point
+    {
+
+        int x;
+        int y;
+        char sym;
+
+        public Point()
+        {
+            Console.WriteLine("Punkt został stworzony.");
+        }
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x=_x;
+            y=_y;
+            sym=_sym;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x,y);
+            Console.Write(sym);
+        }
+
+    }
+}
--- Snake OOP/Snake OOP/Program.cs
+++ Snake OOP/Snake OOP/Program.cs
@@ -10,6 +10,29 @@ namespace Snake_OOP
     {
         static void Main(string[] args)
         {
+            Point p1 = new Point(1,3,'*');
+            Point p2 = new Point(2, 4, '*');
+            Point p3 = new Point(5, 2, '*');
+            Point p4 = new Point(6, 8, '%');
+
+            List<char> charList = new List<char>();
+            charList.Add('*');
+            charList.Add('%');
+            charList.Add('$');
+            charList.Add('#');
+
+            List<Point> plist = new List<Point>();
+            plist.Add(p1);
+            plist.Add(p2);
+            plist.Add(p3);
+            plist.Add(p4);
+
+            foreach (Point p in plist)
+            {
+                p.Draw();
+            }
+
+            Console.ReadLine();
         }
     }
 }
